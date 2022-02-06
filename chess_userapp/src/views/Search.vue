90c3e1f769dad4b5928a4ea805bcb2ad9deddac3
RadovanPrijic <rprijic3619rn@raf.rs>
2022-02-06 19:28:51 +0100

    Dosta razlicitih stvari

---
+++ chess_userapp/src/views/Search.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <Header :subtitle="subtitle"/>
+    <ImageList />
+  </div>
+</template>
+
+<script>
+
+  import Header from '@/components/Header.vue';
+  import ImageList from '@/components/ImageList.vue';
+  import { mapActions } from 'vuex';
+
+  export default {
+    name: 'Search',
+    
+    components: {
+      Header,
+      ImageList
+    },
+
+    data() {
+      return {
+        subtitle: 'Search results: ',
+        imageIDs: null
+      }
+    },
+
+    mounted() {
+      this.search(this.$route.query.q).then( res => {
+        this.subtitle += res;
+      });
+    },
+
+    methods: {
+      ...mapActions([
+        'search'
+      ])
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
