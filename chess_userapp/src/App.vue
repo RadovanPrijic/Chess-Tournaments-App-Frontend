3af46a7b997f44e831d3a776fbb39a62f5be7190
RadovanPrijic <rprijic3619rn@raf.rs>
2022-02-08 16:55:19 +0100

    App i Home modifikacije

--- chess_userapp/src/App.vue
+++ chess_userapp/src/App.vue
@@ -1,32 +1,98 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+
+    <div>
+      <b-navbar toggleable="sm" type="dark" variant="info">
+        <b-navbar-brand to="/">Rezultati šahovskih turnira</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item to="/">Home</b-nav-item>
+            <b-nav-item-dropdown text="Sekcije">
+              <b-nav-item to="/tournaments">Turniri</b-nav-item>
+              <b-nav-item to="/organisers">Organizatori</b-nav-item>
+              <b-nav-item to="/results">Rezultati</b-nav-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item v-if="!token" to="/register">Registracija</b-nav-item>
+            <b-nav-item v-if="!token" to="/login">Prijava</b-nav-item>
+            <b-nav-item v-else @click="logout()">Odjava</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
     </div>
-    <router-view/>
+
+    <router-view class="stranica" />
   </div>
 </template>
 
+<script>
+
+  import { mapState, mapMutations } from 'vuex';
+
+  export default {
+    name: 'App',
+
+    data() {
+      return {
+        searchQuery: ''
+      }
+    },
+
+    computed: {
+      ...mapState([
+        'token'
+      ])
+    },
+
+    mounted() {
+      if (localStorage.token) {
+        this.setToken(localStorage.token);
+      }
+    },
+
+    methods: {
+      ...mapMutations([
+        'setToken',
+        'removeToken'
+      ]),
+
+      search(e) {
+        e.preventDefault();
+      },
+
+      logout() {
+        this.removeToken();
+      }
+    },
+    /*  
+    sockets: {
+      error(err) {
+        alert(err);
+      }
+    } */
+  }
+
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    padding-bottom: 10px;
+  }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  .stranica {
+    width: 80%;
+    margin-left: 10%;
+  }
+  
 </style>
--- chess_userapp/src/views/Home.vue
+++ chess_userapp/src/views/Home.vue
@@ -1,17 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <Header subtitle="Početna stranica"/>
+    <img
+      src="https://i.pinimg.com/736x/3c/4f/18/3c4f1886e5b1d47f3126703fd20f56b7.jpg"
+      width="200"
+      alt="Logo"
+    />
   </div>
 </template>
 
 <script>
+  import Header from '@/components/Header.vue'
 
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'Home',
+    
+    components: {
+      Header
+    }
   }
-}
 </script>
