aaa816bb08aa6aaa2781b80326d51b9e00d22185
RadovanPrijic <rprijic3619rn@raf.rs>
2022-02-08 15:06:28 +0100

    Dosta izmena u Vuex store-u

--- chess_userapp/src/router/index.js
+++ chess_userapp/src/router/index.js
@@ -6,53 +6,82 @@ import Register from '@/views/Register.vue';
 import Tournament from '@/views/Tournament.vue';
 import Organiser from '@/views/Organiser.vue';
 import Result from '@/views/Result.vue';
-import AddResult from "@/views/AddResult.vue";
+import PostResult from "@/views/PostResult.vue";
 import UpdateResult from "@/views/UpdateResult.vue";
+import SingleTournamentView from '@/views/SingleTournamentView.vue';
+import SingleOrganiserView from "@/views/SingleOrganiserView.vue";
+import SingleResultView from "@/views/SingleResultView.vue";
 
 Vue.use(VueRouter)
 
 const routes = [
+
   {
     path: '/',
     name: 'Home',
     component: Home
   },
-  {
-    path: '/login',
-    name: 'Login',
-    component: Login
-  },
+
   {
     path: '/register',
     name: 'Register',
     component: Register
   },
+
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+
   {
     path: '/tournaments',
     name: 'Tournament',
     component: Tournament
   },
+
   {
-    path: '/organiser',
+    path: '/organisers',
     name: 'Organiser',
     component: Organiser
   },
+
   {
     path: '/results/:id',
-    name: 'Result',
+    name: 'ResultList',
     component: Result
   },
+
   {
-    path: '/addresult/:id',
-    name: 'AddResult',
-    component: AddResult
+    path: '/postresult/:id',
+    name: 'PostResult',
+    component: PostResult
   },
+
   {
     path: '/updateresult',
     name: 'UpdateResult',
     component: UpdateResult
   },
 
+  {
+    path: '/tournament/:id',
+    name: 'SingleTournament',
+    component: SingleTournamentView
+  },
+
+  {
+    path: '/organiser/:id',
+    name: 'SingleOrganiser',
+    component: SingleOrganiserView
+  },
+
+  {
+    path: '/result/:id',
+    name: 'SingleResult',
+    component: SingleResultView
+  }
+  
 ]
 
 const router = new VueRouter({
--- chess_userapp/src/store/index.js
+++ chess_userapp/src/store/index.js
@@ -5,11 +5,237 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    token: '',
+    tournament: null,
+    tournaments: [],
+    organiser: null,
+    organisers: [],
+    result: null,
+    results: [],
+    resultData: {
+      id: '',
+      userId: '',
+      loggedUserId: '', // Potencijalno nepotrebno
+      tournamentId: '',
+      ranking: '',
+      prize: '',
+      country_represented: '',
+      elo_change: '',
+      coach: '',
+    }
   },
+
   mutations: {
+    setToken(state, token) {
+      state.token = token;
+      localStorage.token = token;
+    },
+
+    removeToken(state) {
+      state.token = '';
+      localStorage.token = '';
+    },
+
+    setLoggedUserId(state, userId){ // Potencijalno nepotrebno
+      state.loggedUserId = userId;
+    },
+
+    setResultData(state, result){
+      state.resultData.id = result.id,
+      state.resultData.userId = result.userId,
+      state.resultData.tournamentId = result.tournamentId,
+      state.resultData.ranking = result.ranking,
+      state.resultData.prize = result.prize,
+      state.resultData.country_represented = result.country_represented,
+      state.resultData.elo_change = result.elo_change,
+      state.resultData.coach = result.coach
+    },
+
+    setTournaments(state, tournaments) {
+      state.tournaments = tournaments;
+    },
+
+    setOrganisers(state, organisers) {
+      state.organisers = organisers;
+    },
+
+    setResults(state, results) {
+      state.results = results;
+    },
+
+    /*
+    setResultById(state, result) {
+      state.result = result;
+    },
+    */
+
+    addResult(state, result) {
+      state.result = result;
+    },
+
+    addSingleResult(state, result){
+      state.results.push(result);
+    },
+
+    setSelectedResult(state, result){
+      state.result = result;
+    },
+
+    setOrganiserById(state, organiser) {
+      state.organiser = organiser;
+    },
+
+    setTournamentByOrganiserId(state, orgId) {
+      state.tournament = ''
+      state.tournaments.forEach(tournament => {
+        if (tournament.organiserId === orgId){
+          state.tournament = tournament;
+        }
+      });
+    },
   },
+
   actions: {
+
+    register({ commit }, obj) {
+      fetch('http://127.0.0.1:9000/register', { //TODO mozda promeniti rutu u api_register
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(obj)
+      })
+        .then( res => res.json() )
+          .then( tkn => { 
+            if (tkn.msg) {
+              alert(tkn.msg);
+            } else {
+              commit('setToken', tkn.token)
+            }
+          });
+    },
+
+    login({ commit }, obj) {
+      fetch('http://127.0.0.1:9000/login', { //TODO mozda promeniti rutu u api_login
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(obj)
+      })
+        .then( res => res.json() )
+          .then( tkn => { 
+            if (tkn.msg) {
+              alert(tkn.msg);
+            } else {
+              commit('setToken', tkn.token)
+              commit('setLoggedUserId', tkn.userId)
+            }
+          });
+    },
+
+    fetchTournaments({ commit }){
+      fetch('http://127.0.0.1:8500/admin/tournaments',{
+        method: 'GET',
+        headers: {
+            'authorization': `Bearer ${localStorage.token}`
+        },
+    })
+        .then( obj => obj.json() )
+        .then( res => commit('setTournaments', res));
+    },
+
+    fetchOrganisers({ commit }){
+      fetch('http://127.0.0.1:8500/admin/organisers',{
+        method: 'GET',
+        headers: {
+            'authorization': `Bearer ${localStorage.token}`
+        },
+    })
+        .then( obj => obj.json() )
+        .then( res => commit('setOrganisers', res));
+    },
+
+    fetchResults({ commit }){
+      fetch('http://127.0.0.1:8500/admin/results',{
+        method: 'GET',
+        headers: {
+          'authorization': `Bearer ${localStorage.token}`
+        },
+      })
+          .then( obj => obj.json() )
+          .then( res => commit('setResults', res));
+    },
+
+    fetchResultById({ commit }, id){
+      fetch('http://127.0.0.1:8500/admin/results/' + id,{
+        method: 'GET',
+        headers: {
+          'authorization': `Bearer ${localStorage.token}`
+        },
+      })
+          .then( obj => obj.json() )
+          .then( res => commit('addResult', res) );
+    },
+
+    fetchOrganiserById({ commit }, id){
+      fetch('http://127.0.0.1:8500/admin/organisers/' + id,{
+        method: 'GET',
+        headers: {
+          'authorization': `Bearer ${localStorage.token}`
+        },
+      })
+          .then( obj => obj.json() )
+          .then( res => commit('setOrganiserById', res) );
+    },
+
+    fetchTournamentByOrganiserId({ commit }, orgId){
+      fetch('http://127.0.0.1:8500/admin/tournaments',{
+        method: 'GET',
+        headers: {
+          'authorization': `Bearer ${localStorage.token}`
+        }
+      })
+          .then( obj => obj.json() )
+          .then( res => commit('setTournaments', res));
+
+      commit('setTournamentByOrganiserId', orgId);
+    },
+
+    postResult(obj){
+        fetch('http://127.0.0.1:8500/admin/results', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json' ,
+            'authorization': `Bearer ${localStorage.token}`
+          },
+          body: JSON.stringify(obj)
+        }).then( res => res.json() )
+            .then( el => {
+              if (el.msg) {
+                alert(el.msg, 'Došlo je do greško kod dodavanja novog rezultata.');
+              }
+            });
+    },
+
+    updateResult(obj){
+      fetch('http://127.0.0.1:8500/admin/results/' + obj.id, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json' ,
+          'authorization': `Bearer ${localStorage.token}`
+        },
+        body: JSON.stringify(obj)
+      }).then( res => res.json() )
+          .then( el => {
+            if (el.msg) {
+              alert(el.msg, 'Došlo je do greške kod izmene podataka o rezultatu.');
+            }
+          });
+    }
   },
+
   modules: {
+
   }
 })
