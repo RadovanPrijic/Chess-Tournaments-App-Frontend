11910bbf664366a6307b388e23047b253c056636
RadovanPrijic <rprijic3619rn@raf.rs>
2022-02-08 10:54:40 +0100

    Promene u ruteru i druge stvari

--- chess_userapp/src/router/index.js
+++ chess_userapp/src/router/index.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '@/views/Login.vue';
+import Register from '@/views/Register.vue';
+import Tournament from '@/views/Tournament.vue';
+import Organiser from '@/views/Organiser.vue';
+import Result from '@/views/Result.vue';
+import AddResult from "@/views/AddResult.vue";
+import UpdateResult from "@/views/UpdateResult.vue";
 
 Vue.use(VueRouter)
 
@@ -11,13 +18,41 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register
+  },
+  {
+    path: '/tournaments',
+    name: 'Tournament',
+    component: Tournament
+  },
+  {
+    path: '/organiser',
+    name: 'Organiser',
+    component: Organiser
+  },
+  {
+    path: '/results/:id',
+    name: 'Result',
+    component: Result
+  },
+  {
+    path: '/addresult/:id',
+    name: 'AddResult',
+    component: AddResult
+  },
+  {
+    path: '/updateresult',
+    name: 'UpdateResult',
+    component: UpdateResult
+  },
+
 ]
 
 const router = new VueRouter({
